fbc46ad43a48af1f98c68809a51c5a655047d750
qiubangbang <431066566@qq.com>
2017-02-06 10:57:07 +0800

    梳理

--- app/src/main/java/com/qiubangbang/changeskin/MainActivity.java
+++ app/src/main/java/com/qiubangbang/changeskin/MainActivity.java
@@ -38,10 +38,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rlMain = (RelativeLayout) findViewById(R.id.activity_main);
-        //初始化assetmanager 获取 method对象
         Class asset = AssetManager.class;
         try {
             assetManager = (AssetManager) asset.newInstance();
+            /**反射调用addAssetPath隐藏方法（将一个资源添加到assetManage中，可以是文件夹或zip文件）
+             * @param String path 资源文件路径 zip
+             * */
             method = asset.getDeclaredMethod("addAssetPath", String.class);
         } catch (Exception e) {
             e.printStackTrace();
@@ -53,9 +55,19 @@ public class MainActivity extends AppCompatActivity {
         try {
             String pluginPath = getPluginLocation();
             method.invoke(assetManager, pluginPath);
-            //类加载器
+            /***
+             * 类装载器
+             * String dexPath 需要装载的APK或者JAR文件路径。包含多个路径用File.pathSeparator间隔开
+             * ,在Android上默认是":"
+             * String optimizedDirectory 优化后的dex文件存放目录，不能为null,通过创建目录：context.getDir("dex", 0);
+             * 不要把优化优化后的classes文件存放到外部存储设备上，防代码注入攻击。
+             * String librarySearchPath 目标类中使用的C/C++库的列表，每个目录用File.pathSeparator间隔开；
+             * 可以为null,
+             * ClassLoader parent 该类装载器的父装载器，一般用当前执行类的装载器
+             */
             DexClassLoader dexClassLoader = new DexClassLoader(pluginPath, getDir("skinPlugin.apk",
                     Context.MODE_PRIVATE).getAbsolutePath(), null, getClassLoader());
+            /**com.skin.skin_plugin.R$drawable $后面接内部类*/
             Class<?> c = dexClassLoader.loadClass(pluginPackageName + ".R$drawable");
             Field[] fields = c.getDeclaredFields();
             for (Field field : fields) {
